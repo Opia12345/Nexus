c9b36fb14769599615d44e73b36c06f52d77c947
Opia Precious <preciousopia7@gmail.com>
2025-05-05 17:06:29 +0100

    fix: authentication error

--- Client/src/Routes/PrivateRoute.jsx
+++ Client/src/Routes/PrivateRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate } from "react-router-dom";
 import { AuthContext } from "../Context/AuthContext";
 
 const PrivateRoute = ({ children }) => {
-  const userId = JSON.parse(localStorage.getItem("user"))?.userId;
+  const userId = JSON.parse(localStorage.getItem("user"))?.userID;
 
   if (!userId) {
     return <Navigate to="/err" />;
